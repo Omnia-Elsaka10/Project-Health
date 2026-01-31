4e235dbea2e36d1f6112564d35369c322d2dd9e1
OMNIA <omnia.elsaka92@gmail.com>
2026-01-31 06:48:14 +0200

    feat: configure Groq LLM with JSON

--- app/core/llm.py
+++ app/core/llm.py
@@ -1,12 +1,10 @@
-from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq # type: ignore
 from app.core.config import settings
 
 def get_llm():
-    """
-    Initialize and return the Gemini LLM model.
-    """
-    return ChatGoogleGenerativeAI(
-        model=settings.MODEL_NAME,
-        google_api_key=settings.GEMINI_API_KEY,
-        temperature=0.2  # Low temperature for more factual and consistent analysis
+    return ChatGroq(
+        api_key=settings.GROQ_API_KEY,
+        model="llama-3.1-8b-instant",
+        temperature=0,
+        model_kwargs={"response_format": {"type": "json_object"}} 
     )
